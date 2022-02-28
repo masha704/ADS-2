fa6a8776c034e3331ed88d4dd585cc848f5693c0
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-02-28 14:04:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,50 @@
 
 
 double pown(double value, uint16_t n) {
-
+    if (n == 0) {
+        return 1;
+    }
+    double res = value;
+    for (uint16_t i = 0; i < n-1; i++) {
+        res *= value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+    if (n < 0) {
+        return 0;
+    }
+    if (n == 0) {
+        return 1;
+    }
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-
+    if (n < 0) {
+        return 0;
+    }
+    return pown(x, n) / (double)fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    if (count <= 0) {
+        return 0;
+    }
+    return calcItem(x, count - 1) + expn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
-
+    if (count <= 0) {
+        return 0;
+    }
+    return pown(-1, count - 1) * calcItem(x, count * 2 - 1) + sinn(x, count - 1);
 }
 
 double cosn(double x, uint16_t count) {
-
+    if (count <= 0) {
+        return 0;
+    }
+    return pown(-1, count - 1) * calcItem(x, 2 * count - 2) + cosn(x, count - 1);
 }
