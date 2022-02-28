766f6443cef1ca0a90e3729028190fc0617abdf6
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-02-28 14:34:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -33,21 +33,21 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
     if (count <= 0) {
-        return 0;
+        return 1;
     }
-    return calcItem(x, count - 1) + expn(x, count - 1);
+    return calcItem(x, count) + expn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
-    if (count <= 0) {
-        return 0;
+    if (count == 1) {
+        return x;
     }
     return pown(-1, count - 1) * calcItem(x, count * 2 - 1) + sinn(x, count - 1);
 }
 
 double cosn(double x, uint16_t count) {
-    if (count <= 0) {
-        return 0;
+    if (count == 1) {
+        return x;
     }
     return pown(-1, count - 1) * calcItem(x, 2 * count - 2) + cosn(x, count - 1);
 }
