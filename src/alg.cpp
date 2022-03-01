fc365ec4632bdf32656ab47ec794d36a498c2a9e
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-03-01 17:37:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,7 +22,7 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x, n) / (double)fact(n);
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
